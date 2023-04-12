6de8c5c9a4c63e1db9d21fe283cf3fe4c60e84f8
Krittipoom <krittipoom@outlook.com>
2023-04-12 16:45:25 +0700

    update urls py in groups and posts

--- socialproj/groups/urls.py
+++ socialproj/groups/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'groups'
 
 urlpatterns = [
-	path('test/', views.ListGroups.as_view(), name = 'all'),
+	path('', views.ListGroups.as_view(), name = 'all'),
 	path('new/', views.CreateGroups.as_view(), name = 'create'),
 	path('posts/in/<slug:slug>/', views.SingleGroups.as_view(), name = 'single'),
 ]
