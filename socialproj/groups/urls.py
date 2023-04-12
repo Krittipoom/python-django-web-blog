1971719245c229e3be98e8e3bbb472b8db2074da
Krittipoom <krittipoom@outlook.com>
2023-04-12 17:06:47 +0700

    Update urls.py

--- socialproj/groups/urls.py
+++ socialproj/groups/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 	path('', views.ListGroups.as_view(), name = 'all'),
 	path('new/', views.CreateGroups.as_view(), name = 'create'),
 	path('posts/in/<slug:slug>/', views.SingleGroups.as_view(), name = 'single'),
+	path('join/<slug:slug>/', views.JoinGroups.as_view(), name = 'join'),
+	path('leave/<slug:slug>/', views.LeaveGroups.as_view(), name = 'leave'),
 ]
