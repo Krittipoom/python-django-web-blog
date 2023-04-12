66aa9680634a6040edada5b8af5d1d8d369feef8
Krittipoom <krittipoom@outlook.com>
2023-04-12 16:22:45 +0700

    add groups url and GroupMemberInline model

--- socialproj/groups/urls.py
+++ socialproj/groups/urls.py
@@ -1,1 +1,11 @@
 #urls.py
+from django.conf.urls import url
+from . import views
+
+app_name = 'groups'
+
+urlpatterns = [
+	path('test/', views.ListGroups.as_view(), name = 'all'),
+	path('new/', views.CreateGroups.as_view(), name = 'create'),
+	path('posts/in/<slug:slug>/', views.SingleGroups.as_view(), name = 'single'),
+]
